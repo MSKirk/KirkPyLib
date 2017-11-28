068e3933e17e91b9475ba24343a01cbf71b8d088
mskirk <michael.s.kirk@gmail.com>
2017-11-28 16:03:53 -0500

    XML org files

--- AIA_Spikes/Spikes_Stats.py
+++ AIA_Spikes/Spikes_Stats.py
@@ -9,6 +9,7 @@ import numpy as np
 import pandas as pd
 from astropy.io import fits
 from astropy.time import Time
+from scipy import ndimage
 
 
 class Spikes_Stats:
@@ -18,35 +19,60 @@ class Spikes_Stats:
 
         # Read in the expected DB file
         self.spikes_db = pd.read_hdf(self.dir + '/Table_SpikesDB.h5', 'table')
+        # Gropuing in TIme
         self.db_groups()
+
+        # Number of Coincident spikes needed for a positive detection.
+        # !!!!! Set for 2 or more coincidence hits. Change for more stringent parameters. !!!!!
+        self.n_co_spikes = 2.
 
     def db_groups(self):
         # group the spikes into 12 second intervals
-        self.spikes_db['GroupNumber'] = self.spikes_db.groupby(pd.Grouper(key='YMDTime', freq='12s')).ngroup()
+        self.spikes_db['TimeGroup'] = self.spikes_db.groupby(pd.Grouper(key='YMDTime', freq='12s')).ngroup()
+
+    def spike_space_grouping(self,subset):
+        # count through the coincident spikes and label them sequentially...
+        # 3x3 structuring element with connectivity 2
+        struct = ndimage.generate_binary_structure(2, 2)
+
+        spike_filter = np.zeros((4096, 4096))
+
+        for spike_path in subset.Path:
+            spike_filter += ndimage.binary_dilation(np.int_(self.spikes_to_image(spike_path)[0,:,:] > 0), structure=struct).astype(spike_filter.dtype)
+
+        labeled_array, num_features = ndimage.measurements.label(np.int_(spike_filter > (self.n_co_spikes - 1)))
+        return labeled_array
 
     def spikes_dataframe_gen(self, n_sample_groups=False):
 
-        if n_sample_groups == False:
-            n_sample_groups = len(self.spikes_db.GroupNumber.unique())
+        if not n_sample_groups:
+            n_sample_groups = len(self.spikes_db.TimeGroup.unique())
 
-        sample_groups = np.random.choice(self.spikes_db.GroupNumber.unique(), size=n_sample_groups)
-        self.spikes_df = pd.DataFrame(columns={'MJDTime','YMDTime','Wavelength','Pix_i','Pix_j','Int','GroupNumber','Im_int'})
+        sample_groups = np.random.choice(self.spikes_db.TimeGroup.unique(), size=n_sample_groups)
+        self.spikes_df = pd.DataFrame(columns={'MJDTime','YMDTime','Wavelength','Pix_i','Pix_j','Int','TimeGroup','Im_int','SpaceGroup'})
 
         # For each group in our sample, read in the fits files information
+        # Concat into a large dataframe
         for group in sample_groups:
-            for db_index in self.spikes_db.loc[self.spikes_db['GroupNumber'] == group].index:
-                temp_df = pd.DataFrame(columns={'MJDTime','YMDTime','Wavelength','Pix_i','Pix_j','Int','GroupNumber','Im_int'})
+            # might want to move spacial grouping to SpikeVectors...
+            subset = self.spikes_db.loc[self.spikes_db['TimeGroup'] == group]
+
+            self.spacegroup = self.spike_space_grouping(subset)
+
+            for db_index in subset.index:
+                temp_df = pd.DataFrame(columns={'MJDTime','YMDTime','Wavelength','Pix_i','Pix_j','Int','TimeGroup','Im_int','SpaceGroup'})
                 raw_spikes = fits.open(self.spikes_db.Path[db_index])[0].data
 
-                temp_df.Pix_i = raw_spikes[0] % 4096
-                temp_df.Pix_j = np.int_((raw_spikes[0] - raw_spikes[0] % 4096)/4096)
+                temp_df.Pix_j = raw_spikes[0] % 4096
+                temp_df.Pix_i = np.int_((raw_spikes[0] - raw_spikes[0] % 4096)/4096)
                 temp_df.Int = raw_spikes[1]
                 temp_df.Im_int = raw_spikes[2]
 
-                temp_df.GroupNumber = self.spikes_db.GroupNumber[db_index]
+                temp_df.TimeGroup = self.spikes_db.TimeGroup[db_index]
                 temp_df.MJDTime = np.repeat(self.time_gen(self.spikes_db.Path[db_index]).mjd, len(temp_df.Pix_i))
                 temp_df.YMDTime = np.repeat(self.time_gen(self.spikes_db.Path[db_index]).datetime, len(temp_df.Pix_i))
                 temp_df.Wavelength = np.repeat(self.wave_gen(self.spikes_db.Path[db_index]), len(temp_df.Pix_i))
+                temp_df.SpaceGroup = self.spacegroup[temp_df.Pix_i, temp_df.Pix_j]
 
                 self.spikes_df = pd.concat([self.spikes_df,temp_df], ignore_index=True)
 
@@ -58,3 +84,20 @@ class Spikes_Stats:
     def wave_gen(self, filename):
         return np.asarray([filename.split(':')[-1].split('_')[1].split('.')[0]], dtype='uint16')
 
+    def spikes_to_image(self, filename):
+        # read a spike file and return an image with the spikes
+        # Raw_spikes are a (3,N) int32 array with:
+        #   [0,N] pixel location in vector format
+        #   [1,N] pixel value before despike calibration
+        #   [2,N] pixel value at level 1 image after despike
+        #
+        # Returns a [2,4096,4096] array with [0,:,:] with the spike value, and [1,:,:] with the level 1 value
+
+        raw_spikes = fits.open(filename)[0].data
+        spike_vector = np.zeros((4096, 4096), dtype='int32').flatten()
+        lev1_vector = np.zeros_like(spike_vector)
+
+        spike_vector[raw_spikes[0,:]] = raw_spikes[1,:]
+        lev1_vector[raw_spikes[0,:]] = raw_spikes[2,:]
+
+        return np.stack((spike_vector.reshape((4096, 4096)),lev1_vector.reshape((4096, 4096))))
