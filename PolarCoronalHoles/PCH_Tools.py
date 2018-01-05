8012b2288a8e1cf69d54ba37434bba2b9ceb712a
mskirk <michael.s.kirk@gmail.com>
2018-01-05 17:08:53 -0500

    Plot PCH data

--- PolarCoronalHoles/PCH_Tools.py
+++ PolarCoronalHoles/PCH_Tools.py
@@ -14,37 +14,176 @@ A collection of tools used in PCH detection. This replicates the following IDL r
 '''
 
 
-
-
-def trigfit(self, theta, rho):
-    tt = numpy.array(theta)
-    yy = numpy.array(rho)
+def trigfit(theta, rho, sigma=None, degree=1):
+    tt = np.array(theta)
+    yy = np.array(rho)
     # assume uniform spacing
-    ff = numpy.fft.fftfreq(len(tt), (tt[1] - tt[0]))
-    Fyy = abs(numpy.fft.fft(yy))
+    ff = np.fft.fftfreq(len(tt), (tt[1] - tt[0]))
+    Fyy = abs(np.fft.fft(yy))
 
     # excluding the zero frequency "peak", which is related to offset
-    guess_freq = abs(ff[numpy.argmax(Fyy[1:]) + 1])
-    guess_amp = numpy.std(yy) * 2. ** 0.5
-    guess_offset = numpy.mean(yy)
-    guess = numpy.array([guess_offset, guess_amp, 2. * numpy.pi * guess_freq])
+    guess_freq = abs(ff[np.argmax(Fyy[1:]) + 1])
+    guess_amp = np.std(yy) * 2. ** 0.5
+    guess_offset = np.mean(yy)
 
-    def cos_series(t,A, degree=1):
-        '''
-        :param t: independent variables
-        :param A: coefficients
-        :param degree: degree of the function
-        :return: function
-        '''
-        F = A[0]
-        for n in range(1, degree+1):
-            cx=np.cos(n*t + A[2*n])
-            F += cx*A[2*n-1]
-        return F
+    # Guesses only first order cosine fit
+    guess = np.append(np.array([guess_offset, guess_amp, 2. * np.pi * guess_freq]), np.zeros((degree-1)*2))
 
-    popt, pcov = optimize.curve_fit(cos_series, tt, yy, p0=guess)
-    A, w, p, c = popt
-    f = w / (2. * numpy.pi)
-    fitfunc = lambda t: A * numpy.sin(w * t + p) + c
-    return {"amp": A, "omega": w, "phase": p, "offset": c, "freq": f, "period": 1. / f, "fitfunc": fitfunc,
-            "maxcov": numpy.max(pcov), "rawres": (guess, popt, pcov)}
+    if degree == 1:
+        def cos_series(t, c, a1, p1):
+            F = c
+            F += a1*np.cos(1 * t + p1)
+            return F
+
+    if degree == 2:
+        def cos_series(t, c, a1, p1, a2, p2):
+            F = c
+            F += a1 * np.cos(1 * t + p1)
+            F += a2 * np.cos(2 * t + p2)
+            return F
+
+    if degree == 3:
+        def cos_series(t, c, a1, p1, a2, p2, a3, p3):
+            F = c
+            F += a1 * np.cos(1 * t + p1)
+            F += a2 * np.cos(2 * t + p2)
+            F += a3 * np.cos(3 * t + p3)
+            return F
+
+    if degree == 4:
+        def cos_series(t, c, a1, p1, a2, p2, a3, p3, a4, p4):
+            F = c
+            F += a1 * np.cos(1 * t + p1)
+            F += a2 * np.cos(2 * t + p2)
+            F += a3 * np.cos(3 * t + p3)
+            F += a4 * np.cos(4 * t + p4)
+            return F
+
+    if degree == 5:
+        def cos_series(t, c, a1, p1, a2, p2, a3, p3, a4, p4, a5, p5):
+            F = c
+            F += a1 * np.cos(1 * t + p1)
+            F += a2 * np.cos(2 * t + p2)
+            F += a3 * np.cos(3 * t + p3)
+            F += a4 * np.cos(4 * t + p4)
+            F += a5 * np.cos(5 * t + p5)
+            return F
+
+    if degree == 6:
+        def cos_series(t, c, a1, p1, a2, p2, a3, p3, a4, p4, a5, p5, a6, p6):
+            F = c
+            F += a1 * np.cos(1 * t + p1)
+            F += a2 * np.cos(2 * t + p2)
+            F += a3 * np.cos(3 * t + p3)
+            F += a4 * np.cos(4 * t + p4)
+            F += a5 * np.cos(5 * t + p5)
+            F += a6 * np.cos(6 * t + p6)
+            return F
+
+    if degree == 7:
+        def cos_series(t, c, a1, p1, a2, p2, a3, p3, a4, p4, a5, p5, a6, p6, a7, p7):
+            F = c
+            F += a1 * np.cos(1 * t + p1)
+            F += a2 * np.cos(2 * t + p2)
+            F += a3 * np.cos(3 * t + p3)
+            F += a4 * np.cos(4 * t + p4)
+            F += a5 * np.cos(5 * t + p5)
+            F += a6 * np.cos(6 * t + p6)
+            F += a7 * np.cos(7 * t + p7)
+            return F
+
+    if degree == 8:
+        def cos_series(t, c, a1, p1, a2, p2, a3, p3, a4, p4, a5, p5, a6, p6, a7, p7, a8, p8):
+            F = c
+            F += a1 * np.cos(1 * t + p1)
+            F += a2 * np.cos(2 * t + p2)
+            F += a3 * np.cos(3 * t + p3)
+            F += a4 * np.cos(4 * t + p4)
+            F += a5 * np.cos(5 * t + p5)
+            F += a6 * np.cos(6 * t + p6)
+            F += a7 * np.cos(7 * t + p7)
+            F += a8 * np.cos(8 * t + p8)
+            return F
+
+    if degree == 9:
+        def cos_series(t, c, a1, p1, a2, p2, a3, p3, a4, p4, a5, p5, a6, p6, a7, p7, a8, p8, a9, p9):
+            F = c
+            F += a1 * np.cos(1 * t + p1)
+            F += a2 * np.cos(2 * t + p2)
+            F += a3 * np.cos(3 * t + p3)
+            F += a4 * np.cos(4 * t + p4)
+            F += a5 * np.cos(5 * t + p5)
+            F += a6 * np.cos(6 * t + p6)
+            F += a7 * np.cos(7 * t + p7)
+            F += a8 * np.cos(8 * t + p8)
+            F += a9 * np.cos(9 * t + p9)
+            return F
+
+    if degree == 10:
+        def cos_series(t, c, a1, p1, a2, p2, a3, p3, a4, p4, a5, p5, a6, p6, a7, p7, a8, p8, a9, p9, a10, p10):
+            F = c
+            F += a1 * np.cos(1 * t + p1)
+            F += a2 * np.cos(2 * t + p2)
+            F += a3 * np.cos(3 * t + p3)
+            F += a4 * np.cos(4 * t + p4)
+            F += a5 * np.cos(5 * t + p5)
+            F += a6 * np.cos(6 * t + p6)
+            F += a7 * np.cos(7 * t + p7)
+            F += a8 * np.cos(8 * t + p8)
+            F += a9 * np.cos(9 * t + p9)
+            F += a10 * np.cos(10 * t + p10)
+            return F
+
+    popt, pcov = optimize.curve_fit(cos_series, tt, yy, p0=guess.tolist(), sigma=sigma)
+
+    if degree == 1:
+        fitfunc = lambda t: popt[0] + popt[1] * np.cos(1 * t + popt[2])
+
+    if degree == 2:
+        fitfunc = lambda t: popt[0] + popt[1] * np.cos(1 * t + popt[2]) + popt[3] * np.cos(2 * t + popt[4])
+
+    if degree == 3:
+        fitfunc = lambda t: popt[0] + popt[1] * np.cos(1 * t + popt[2]) + popt[3] * np.cos(2 * t + popt[4]) \
+                            + popt[5] * np.cos(3 * t + popt[6])
+
+    if degree == 4:
+        fitfunc = lambda t: popt[0] + popt[1] * np.cos(1 * t + popt[2]) + popt[3] * np.cos(2 * t + popt[4]) \
+                            + popt[5] * np.cos(3 * t + popt[6]) + popt[7] * np.cos(4 * t + popt[8])
+
+    if degree == 5:
+        fitfunc = lambda t: popt[0] + popt[1] * np.cos(1 * t + popt[2]) + popt[3] * np.cos(2 * t + popt[4]) \
+                            + popt[5] * np.cos(3 * t + popt[6]) + popt[7] * np.cos(4 * t + popt[8]) \
+                            + popt[9] * np.cos(5 * t + popt[10])
+
+    if degree == 6:
+        fitfunc = lambda t: popt[0] + popt[1] * np.cos(1 * t + popt[2]) + popt[3] * np.cos(2 * t + popt[4]) \
+                            + popt[5] * np.cos(3 * t + popt[6]) + popt[7] * np.cos(4 * t + popt[8]) \
+                            + popt[9] * np.cos(5 * t + popt[10]) + popt[11] * np.cos(6 * t + popt[12])
+
+    if degree == 7:
+        fitfunc = lambda t: popt[0] + popt[1] * np.cos(1 * t + popt[2]) + popt[3] * np.cos(2 * t + popt[4]) \
+                            + popt[5] * np.cos(3 * t + popt[6]) + popt[7] * np.cos(4 * t + popt[8]) \
+                            + popt[9] * np.cos(5 * t + popt[10]) + popt[11] * np.cos(6 * t + popt[12]) \
+                            + popt[13] * np.cos(7 * t + popt[14])
+
+    if degree == 8:
+        fitfunc = lambda t: popt[0] + popt[1] * np.cos(1 * t + popt[2]) + popt[3] * np.cos(2 * t + popt[4]) \
+                            + popt[5] * np.cos(3 * t + popt[6]) + popt[7] * np.cos(4 * t + popt[8]) \
+                            + popt[9] * np.cos(5 * t + popt[10]) + popt[11] * np.cos(6 * t + popt[12]) \
+                            + popt[13] * np.cos(7 * t + popt[14])+ popt[15] * np.cos(8 * t + popt[16])
+
+    if degree == 9:
+        fitfunc = lambda t: popt[0] + popt[1] * np.cos(1 * t + popt[2]) + popt[3] * np.cos(2 * t + popt[4]) \
+                            + popt[5] * np.cos(3 * t + popt[6]) + popt[7] * np.cos(4 * t + popt[8]) \
+                            + popt[9] * np.cos(5 * t + popt[10]) + popt[11] * np.cos(6 * t + popt[12]) \
+                            + popt[13] * np.cos(7 * t + popt[14]) + popt[15] * np.cos(8 * t + popt[16]) \
+                            + popt[17] * np.cos(9 * t + popt[18])
+
+    if degree == 10:
+        fitfunc = lambda t: popt[0] + popt[1] * np.cos(1 * t + popt[2]) + popt[3] * np.cos(2 * t + popt[4]) \
+                            + popt[5] * np.cos(3 * t + popt[6]) + popt[7] * np.cos(4 * t + popt[8]) \
+                            + popt[9] * np.cos(5 * t + popt[10]) + popt[11] * np.cos(6 * t + popt[12]) \
+                            + popt[13] * np.cos(7 * t + popt[14]) + popt[15] * np.cos(8 * t + popt[16]) \
+                            + popt[17] * np.cos(9 * t + popt[18]) + popt[19] * np.cos(10 * t + popt[20])
+
+    return {'popt': popt, 'pcov': pcov, 'fitfunc': fitfunc}
