8d8ed455ed105952c48c7f9969e0db2839920d1e
mskirk <michael.s.kirk@gmail.com>
2018-02-28 15:27:36 -0500

    Creating timing benchmarks for optimization.

--- AIA_Spikes/SpikeVectors_benchmarking.py
+++ AIA_Spikes/SpikeVectors_benchmarking.py
@@ -3,6 +3,7 @@ from AIA_Spikes import SpikeVectors as sv
 import os
 import pandas as pd
 import numpy as np
+import astropy.units as u
 
 
 def io_benchmark(directory):
@@ -21,6 +22,7 @@ def io_benchmark(directory):
     print('Over %0.0f files.' %(len(ffiles)-1))
     print('It takes an average of %0.3f seconds for file I/O.' %np.mean(times).total_seconds())
 
+    return np.mean(times).total_seconds()
 
 def filesize_benchmark():
 
@@ -40,14 +42,23 @@ def filesize_benchmark():
 
 
 def sort_spikes_benchmark():
+
+    init_time = initialization_benchmark()
+
     startTime = datetime.now()
     sv.Sort_Spikes('/Volumes/BigSolar/AIA_Spikes', end_group=48)
     timedif = datetime.now()-startTime
     ngroups = (48/8)+1
 
+    io_time = io_benchmark('/Volumes/BigSolar/Filtered_Spikes/2010/05/13')
+
+    time_per_file = ((timedif.total_seconds()-init_time)/(ngroups * 7))
+
     print('Processing over %0.0f groups:' %ngroups)
     print('Total processing time %0.1f seconds;' %timedif.total_seconds())
-    print('Averaging %0.1f seconds per spike file (not counting initialization).' %((timedif.total_seconds()-61.3)/(ngroups * 7)))
+    print('Averaging %0.3f seconds per spike file (not counting initialization);' %time_per_file)
+    print('Averaging %0.3f seconds of processing per spike file;' %(time_per_file-io_time))
+    print('And a projected processing time of %0.0f days to process all spikes' %((time_per_file*131198781*u.s).to('d')))
 
 
 def initialization_benchmark():
@@ -62,5 +73,7 @@ def initialization_benchmark():
     timedif = datetime.now() - startTime
 
     print('Total initialization time: %0.2f seconds;' %timedif.total_seconds())
+
+    return timedif.total_seconds()
 
 
