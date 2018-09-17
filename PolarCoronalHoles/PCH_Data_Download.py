756f4e48d1c4e14a1b9eb9b7e43ae6fa2a428999
MSKirk <michael.s.kirk@gmail.com>
2018-09-17 12:02:14 -0400

    Restructured data download to deal with the VSO/FIDO issues.

--- PolarCoronalHoles/PCH_Data_Download.py
+++ PolarCoronalHoles/PCH_Data_Download.py
@@ -2,49 +2,75 @@ import astropy.units as u
 from sunpy.net import Fido, attrs as a
 import os
 import numpy as np
+import random
+import fnmatch
 
 
-def aia_pch_data_download(rootpath=''):
+def aia_pch_data_download(rootpath='', waves=[171,193,304]):
 
     blank_results = Fido.search(a.Time('2030/1/1', '2030/1/2'), a.Instrument('aia'))
 
-    for waves in [171,193,304]:
+    for wave in waves:
 
         if not rootpath:
             path = os.path.abspath(os.path.curdir)
         else:
-            path = os.path.abspath(rootpath)+'/'+np.str(waves)
+            path = os.path.abspath(rootpath)+'/'+np.str(wave)
 
         results = blank_results
 
         while results.file_num == 0:
-            results = Fido.search(a.Time('2016/1/1', '2018/6/30'), a.Instrument('aia'), a.Wavelength(waves * u.angstrom), a.vso.Sample(3 * u.hour))
+            results = Fido.search(a.Time('2016/1/1', '2018/6/30'), a.Instrument('aia'), a.Wavelength(wave * u.angstrom), a.vso.Sample(3 * u.hour))
 
-        downloaded_files = Fido.fetch(results, path=path+'/'+np.str(waves)+'/{file}')
+        expected_number = 7210.
+        neededfileindex = []
 
-        if results.file_num == len(downloaded_files):
+        if (results.file_num/expected_number) > 0.98:
+            file_order = random.sample(range(results.file_num), results.file_num)
+
+            for ii, file_number in enumerate(file_order):
+                downloaded_file = Fido.fetch(results[0, file_number], path=path+'/{file}')
+                print(int((ii/results.file_num)*10000.)/100.)
+
+                if not downloaded_file:
+                    neededfileindex += [file_number]
+
+        if results.file_num == len(fnmatch.filter(os.listdir(path), '*.fits')):
             print('Download between 2016/1/1 and 2018/6/30 successful')
         else:
             print('Expected '+np.str(results.file_num)+' files.')
-            print('Got '+np.str(len(downloaded_files))+' files.')
+            print('Got '+np.str(len(fnmatch.filter(os.listdir(path), '*.fits')))+' files.')
 
-            with open(path+'/'+np.str(waves)+'filelist_16-18.txt', 'w') as f:
-                for item in downloaded_files:
+            with open(path+'/'+np.str(wave)+'NeededFileIndex_16-18.txt', 'w') as f:
+                for item in neededfileindex:
                     f.write("%s\n" % item)
+
+        # ----------------------------------------------------
 
         results = blank_results
 
         while results.file_num == 0:
-            results = Fido.search(a.Time('2010/1/1', '2015/12/31'), a.Instrument('aia'), a.Wavelength(waves * u.angstrom), a.vso.Sample(3 * u.hour))
+            results = Fido.search(a.Time('2010/1/1', '2015/12/31'), a.Instrument('aia'), a.Wavelength(wave * u.angstrom), a.vso.Sample(3 * u.hour))
 
-        downloaded_files = Fido.fetch(results, path=path+'/'+np.str(waves)+'/{file}')
+        expected_number = 16456.
+        neededfileindex = []
 
-        if results.file_num == len(downloaded_files):
+        if (results.file_num/expected_number) > 0.98:
+            file_order = random.sample(range(results.file_num), results.file_num)
+
+            for ii, file_number in enumerate(file_order):
+                downloaded_file = Fido.fetch(results[0, file_number], path=path+'/{file}')
+                print(int((ii/results.file_num)*10000.)/100.)
+
+                if not downloaded_file:
+                    neededfileindex += [file_number]
+
+        if results.file_num == len(fnmatch.filter(os.listdir(path), '*.fits')):
             print('Download between 2010/1/1 and 2015/6/30 successful')
         else:
             print('Expected '+np.str(results.file_num)+' files.')
-            print('Got '+np.str(len(downloaded_files))+' files.')
+            print('Got '+np.str(len(fnmatch.filter(os.listdir(path), '*.fits')))+' files.')
 
-            with open(path+'/'+np.str(waves)+'filelist_10-15.txt', 'w') as f:
-                for item in downloaded_files:
+            with open(path+'/'+np.str(wave)+'NeededFileIndex_10-15.txt', 'w') as f:
+                for item in neededfileindex:
                     f.write("%s\n" % item)
