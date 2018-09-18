84c1f21dc24cb35a1d0315bba7576b362c82f9bc
MSKirk <michael.s.kirk@gmail.com>
2018-09-18 12:53:26 -0400

    Forced JSOC source

--- PolarCoronalHoles/PCH_Data_Download.py
+++ PolarCoronalHoles/PCH_Data_Download.py
@@ -4,10 +4,12 @@ import os
 import numpy as np
 import random
 import fnmatch
+import time
 
 
 def aia_pch_data_download(rootpath='', waves=[171,193,304]):
 
+    client = jsoc.JSOCClient()
     blank_results = Fido.search(a.Time('2030/1/1', '2030/1/2'), a.Instrument('aia'))
 
     for wave in waves:
@@ -19,29 +21,38 @@ def aia_pch_data_download(rootpath='', waves=[171,193,304]):
 
         results = blank_results
 
-        while results.file_num == 0:
-            results = Fido.search(a.jsoc.Time('2016/1/1', '2018/6/30'), a.jsoc.Notify('michael.s.kirk@nasa.gov'),
+        while len(results.table) == 0:
+            results = client.search(a.jsoc.Time('2016/1/1', '2018/6/30'), a.jsoc.Notify('michael.s.kirk@nasa.gov'),
                                   a.jsoc.Series('aia.lev1_euv_12s'), a.jsoc.Wavelength(wave * u.angstrom),
                                   a.Sample(3 * u.hour))
 
         expected_number = 7210.
         neededfileindex = []
 
-        if (results.file_num/expected_number) > 0.98:
-            file_order = random.sample(range(results.file_num), results.file_num)
+#
+#        if (len(results.table) / expected_number) > 0.98:
+#            while requests.status != 0:
+#                requests = client.request_data(results)
+#                if requests.status != 0:
+#                    time.sleep(60*5.)
+
+
+
+        if (len(results.table)/expected_number) > 0.98:
+            file_order = random.sample(range(len(results.table)), len(results.table))
 
             for ii, file_number in enumerate(file_order):
                 downloaded_file = Fido.fetch(results[0, file_number], path=path+'/{file}')
-                print(int((ii/results.file_num)*10000.)/100.)
+                print(int((ii/len(results.table))*10000.)/100.)
 
                 if not downloaded_file:
                     print('Error in file number '+np.str(file_number))
                     neededfileindex += [file_number]
 
-        if results.file_num == len(fnmatch.filter(os.listdir(path), '*.fits')):
+        if len(results.table) == len(fnmatch.filter(os.listdir(path), '*.fits')):
             print('Download between 2016/1/1 and 2018/6/30 successful')
         else:
-            print('Expected '+np.str(results.file_num)+' files.')
+            print('Expected '+np.str(len(results.table))+' files.')
             print('Got '+np.str(len(fnmatch.filter(os.listdir(path), '*.fits')))+' files.')
 
             with open(path+'/'+np.str(wave)+'NeededFileIndex_16-18.txt', 'w') as f:
@@ -52,29 +63,29 @@ def aia_pch_data_download(rootpath='', waves=[171,193,304]):
 
         results = blank_results
 
-        while results.file_num == 0:
-            results = Fido.search(a.jsoc.Time('2010/1/1', '2015/12/31'), a.jsoc.Notify('michael.s.kirk@nasa.gov'),
+        while len(results.table) == 0:
+            results = client.search(a.jsoc.Time('2010/1/1', '2015/12/31'), a.jsoc.Notify('michael.s.kirk@nasa.gov'),
                                   a.jsoc.Series('aia.lev1_euv_12s'), a.jsoc.Wavelength(wave * u.angstrom),
                                   a.Sample(3 * u.hour))
 
         expected_number = 16456.
         neededfileindex = []
 
-        if (results.file_num/expected_number) > 0.98:
-            file_order = random.sample(range(results.file_num), results.file_num)
+        if (len(results.table)/expected_number) > 0.98:
+            file_order = random.sample(range(len(results.table)), len(results.table))
 
             for ii, file_number in enumerate(file_order):
                 downloaded_file = Fido.fetch(results[0, file_number], path=path+'/{file}')
-                print(int((ii/results.file_num)*10000.)/100.)
+                print(int((ii/len(results.table))*10000.)/100.)
 
                 if not downloaded_file:
                     print('Error in file number ' + np.str(file_number))
                     neededfileindex += [file_number]
 
-        if results.file_num == len(fnmatch.filter(os.listdir(path), '*.fits')):
+        if len(results.table) == len(fnmatch.filter(os.listdir(path), '*.fits')):
             print('Download between 2010/1/1 and 2015/6/30 successful')
         else:
-            print('Expected '+np.str(results.file_num)+' files.')
+            print('Expected '+np.str(len(results.table))+' files.')
             print('Got '+np.str(len(fnmatch.filter(os.listdir(path), '*.fits')))+' files.')
 
             with open(path+'/'+np.str(wave)+'NeededFileIndex_10-15.txt', 'w') as f:
