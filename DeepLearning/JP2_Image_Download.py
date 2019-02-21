2a46c5ee5ae6eaa94a1fcfabf2b13693be9cf5b9
Michael Kirk <michael.s.kirk@nasa.gov>
2019-02-21 12:43:41 -0500

    add in comments and clean some code

--- DeepLearning/JP2_Image_Download.py
+++ DeepLearning/JP2_Image_Download.py
@@ -7,7 +7,7 @@ import astropy.units as u
 from astropy.coordinates import SkyCoord
 
 import numpy as np
-from scipy.misc import imsave
+from imageio import imwrite
 
 import datetime
 import os
@@ -16,6 +16,14 @@ import os
 class Jp2ImageDownload:
 
     def __init__(self, save_dir='', full_image_set=False, tstart='2012/05/30 23:59:59', tend='2012/05/31 23:59:59'):
+        """
+        Set up initial start and ending times and save directory.
+
+        :param save_dir: Directory to contain the full subdirectory tree of images and masks
+        :param full_image_set: boolean if true will run through the entire SDO catalogue
+        :param tstart: starting time string of any format read by parse_time
+        :param tend: ending time string of any format read by parse_time
+        """
 
         if full_image_set:
             tstart = '2010/05/31 23:59:59'
@@ -26,15 +34,17 @@ class Jp2ImageDownload:
         else:
             self.save_dir = os.path.abspath(os.curdir)
 
-        self.date_format = '%Y/%m/%d %H:%M:%S'
-
-        self.tstart = datetime.datetime.strptime(tstart, self.date_format)
-        self.tend = datetime.datetime.strptime(tend, self.date_format)
+        self.tstart = parse_time(tstart)
+        self.tend = parse_time(tend)
 
         self.date_list = self.gen_date_list()
-        self.date_string_list = [tt.strftime(self.date_format) for tt in self.date_list]
+        self.date_string_list = [tt.strftime('%Y/%m/%d %H:%M:%S') for tt in self.date_list]
 
     def download_images(self):
+        """
+        Run through the complete set of dates as determined in the self.__init__ call by day
+        Sets up a subdirectory tree to organize the images
+        """
 
         for ii, download_date in enumerate(self.date_list):
             directories = download_date.strftime("%Y/%m/%d")
@@ -43,10 +53,15 @@ class Jp2ImageDownload:
 
             self.get_feature_images(self.date_string_list[ii + 1], self.date_string_list[ii], save_path)
 
-        return True
 
     def get_all_sdo_images(self, time_in, save_path=''):
-        # Get a complete set of the SDO images in AIA and HMI for a given time
+        """
+        Download a complete set of the SDO images in AIA and HMI for a given time
+
+        :param time_in: string time for JP2 image download
+        :param save_path: save path for downloaded images
+        :return: full file path of example AIA image downloaded (335 channel)
+        """
 
         hv = helioviewer.HelioviewerClient()
 
@@ -64,6 +79,14 @@ class Jp2ImageDownload:
         return aia_filepath
 
     def get_feature_images(self, time_start, time_end, save_dir):
+        """
+        Download jp2 SDO images and create feature masks for a given time period
+
+        :param time_start: String start time for HEK query
+        :param time_end: string end time for HEK query
+        :param save_dir: directory where images are saved
+        :return:
+        """
 
         client = hek.HEKClient()
         result = client.search(hek.attrs.Time(time_start, time_end), hek.attrs.FRM.Name == 'SPoCA')  # CH and AR
@@ -87,16 +110,29 @@ class Jp2ImageDownload:
             self.write_feature_mask(ss_mask, time_in, 'SS', save_path=save_dir)
 
     def gen_date_list(self):
+        """
+        Create list of datetimes by day
 
+        :return: list of datetimes broken into day segments
+        """
         time_between = self.tend - self.tstart
         return [self.tend - datetime.timedelta(days=dd) for dd in range(0, time_between.days + 1)]
 
     def gen_feature_mask(self, feature_time, feature_list, image_filepath):
+        """
+        Create a binary mask of feature locations
+
+        :param feature_time: Time string of the feature detection
+        :param feature_list: List of HEK dictionaries
+        :param image_filepath: file path of image in which the feature is detected
+        :return: binary mask of feature
+        """
 
         aia_map = Map(image_filepath)
 
         mask = np.zeros([4096,4096])
 
+        # parsing boundary coordinate string for each feature
         for feature in feature_list:
             p1 = feature["hpc_boundcc"][9:-2]
             p2 = p1.split(',')
@@ -106,18 +142,30 @@ class Jp2ImageDownload:
             feature_boundary = SkyCoord([(float(v[0]), float(v[1])) * u.arcsec for v in p3], obstime=feature_date,
                                         frame=frames.Helioprojective)
 
-            # Contour of feature
-            pixel_contour = feature_boundary.to_pixel(aia_map.wcs)
-            mask[np.round(pixel_contour[0]).astype('int'), np.round(pixel_contour[1]).astype('int')] = True
+            # Vertices  of feature
+            pixel_vertex = feature_boundary.to_pixel(aia_map.wcs)
+            mask[np.round(pixel_vertex[0]).astype('int'), np.round(pixel_vertex[1]).astype('int')] = True
+
+            # Need to add in contours between vertices
+            # Need to add in feature filling in mask
 
         return mask
 
     def write_feature_mask(self, mask_in, feature_time, feature_type='Mask', save_path=''):
+        """
+        Save the feature mask as an image
+
+        :param mask_in: Mask of feature as np array
+        :param feature_time: time of the feature detection as DateTime
+        :param feature_type: String of feature type e.g. "SS", "CH", or "AR"
+        :param save_path: save path for saving mask as image
+        :return: none
+        """
 
         feature_date = parse_time(feature_time)
 
         feature_mask_name = feature_date.strftime("%Y_%m_%d__%H_%M_%S")+'__'+feature_type+'_mask.jp2'
         save_mask_name = os.path.join(save_path, feature_mask_name)
 
-        imsave(save_mask_name, mask_in.astype('uint8'))
+        imwrite(save_mask_name, mask_in.astype('uint8'))
 
