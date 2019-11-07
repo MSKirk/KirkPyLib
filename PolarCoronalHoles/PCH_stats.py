756ce936b37c06b1010b54bed0219df86203bb69
MSKirk <michael.s.kirk@gmail.com>
2019-11-07 13:21:19 -0500

    add ccombine_stats and read_all_df

--- PolarCoronalHoles/PCH_stats.py
+++ PolarCoronalHoles/PCH_stats.py
@@ -3,6 +3,7 @@ import astropy.stats.circstats as cs
 from scipy.stats import circmean, circstd
 import matplotlib.pyplot as plt
 import astropy.units as u
+from astropy.time import Time
 import pandas as pd
 import numpy as np
 
@@ -100,6 +101,51 @@ def circular_rebinning(pch_obj, binsize=10):
     return pd.DataFrame(data=bin_stats)
 
 
+def aggregation_rebinning(hole_stats, binsize=10):
+    # Aggregate mission dataframes like those returned by chole_stats
+
+
+    # Aggregation Rules
+    n_agg = {
+        'N_lon_mean': {
+            '': lambda x: circmean(x, low=0, high=360)},
+        'N_lon_std':{
+            '': lambda x: np.sqrt(np.nansum(x**2))},
+        'N_lat_mean': {
+            '': lambda x: circmean(x, low=-90, high=90)},
+        'N_lat_std': {
+            '': lambda x: np.sqrt(np.nansum(x ** 2))},
+        'N_mean_area':{
+            'agg': 'mean'},
+        }
+
+    s_agg = {
+        'S_lon_mean': {
+            '': lambda x: circmean(x, low=0, high=360)},
+        'S_lon_std': {
+            '': lambda x: np.sqrt(np.nansum(x ** 2))},
+        'S_lat_mean': {
+            '': lambda x: circmean(x, low=-90, high=90)},
+        'S_lat_std': {
+            '': lambda x: np.sqrt(np.nansum(x ** 2))},
+        'S_mean_area': {
+            'agg': 'mean'},
+        }
+    
+    northern = hole_stats.groupby(hole_stats['N_lon_mean'].apply(lambda x: np.round(x / binsize))).agg(n_agg)
+    northern.columns = ["".join(x) for x in northern.columns.ravel()]
+
+    southern = hole_stats.groupby(hole_stats['S_lon_mean'].apply(lambda x: np.round(x / binsize))).agg(s_agg)
+    southern.columns = ["".join(x) for x in southern.columns.ravel()]
+
+    bin_stats = pd.concat([northern, southern], join='outer', axis=1)
+
+    values = {'S_lon_std': 0,  'N_lon_std': 0,  'S_lat_std': 0,  'N_lat_std': 0}
+    bin_stats = bin_stats.fillna(value=values).fillna(method='ffill').fillna(method='bfill')
+
+    return bin_stats
+    
+    
 def areaint(lats, lons):
 
     assert lats.size == lons.size, 'List of latitudes and longitudes are different sizes.'
@@ -212,7 +258,7 @@ def chole_stats(pch_obj, wav_filter, binsize=10, sigma=1):
 
         binned = circular_rebinning(one_rot, binsize=binsize)
 
-        h_loc = np.argmin(np.abs((binned.index.values*binsize) - one_rot.Harvey_Longitude[0]))
+        h_loc = np.argmin(np.abs((binned.index.values*binsize) - one_rot.Harvey_Longitude.iloc[0]))
         hole_stats['N_lon_mean'][hr] = binned.N_Lon_Mean.iloc[h_loc] * u.deg
         hole_stats['N_lon_std'][hr] = binned.N_Lon_Std.iloc[h_loc] * u.deg
         hole_stats['N_lat_mean'][hr] = binned.N_Lat_Mean.iloc[h_loc] * u.deg
@@ -234,6 +280,88 @@ def chole_stats(pch_obj, wav_filter, binsize=10, sigma=1):
         hole_stats['Harvey_Rotation'][hr] = hr
         print(np.int(((hr-measurements.Harvey_Rotation[0])/(measurements.Harvey_Rotation[-1]-measurements.Harvey_Rotation[0]))*100.))
 
+    hole_stats['Filter'] = wav_filter
+
     return hole_stats
 
 
+def combine_stats(pch_dfs, sigma=1, binsize=10):
+    # pch_dfs = a list of dataframes to combine
+
+    all_stats = pd.concat(pch_dfs).sort_index()
+
+    hole_stats = pd.DataFrame(index=all_stats.Harvey_Rotation.unique(), columns=['Harvey_Rotation',
+                            'Date', 'N_mean_area', 'S_mean_area', 'N_min_area','S_min_area', 'N_max_area','S_max_area',
+                            'N_center_lat','N_center_lon', 'S_center_lat','S_center_lon','N_lat_mean', 'N_lat_std',
+                            'N_lon_mean', 'N_lon_std','S_lat_mean', 'S_lat_std', 'S_lon_mean', 'S_lon_std',
+                                                                                 'N_mean_area_agg','S_mean_area_agg'])
+
+    for hr in all_stats.Harvey_Rotation.unique():
+        one_rot = one_hr_select(all_stats, hr)
+
+        binned = aggregation_rebinning(remove_quantities(one_rot), binsize=binsize)
+
+        h_loc = np.argmin(np.abs((binned.index.values * binsize) - PCH_Tools.get_harvey_lon(Time(one_rot.Date.iloc[0])).value))
+        hole_stats['N_lon_mean'][hr] = binned.N_lon_mean.iloc[h_loc] * u.deg
+        hole_stats['N_lon_std'][hr] = binned.N_lon_std.iloc[h_loc] * u.deg
+        hole_stats['N_lat_mean'][hr] = binned.N_lat_mean.iloc[h_loc] * u.deg
+        hole_stats['N_lat_std'][hr] = binned.N_lat_std.iloc[h_loc] * u.deg
+        hole_stats['N_mean_area_agg'][hr] = binned.N_mean_areaagg.iloc[h_loc]
+
+        hole_stats['S_lon_mean'][hr] = binned.S_lon_mean.iloc[h_loc] * u.deg
+        hole_stats['S_lon_std'][hr] = binned.S_lon_std.iloc[h_loc] * u.deg
+        hole_stats['S_lat_mean'][hr] = binned.S_lat_mean.iloc[h_loc] * u.deg
+        hole_stats['S_lat_std'][hr] = binned.S_lat_std.iloc[h_loc] * u.deg
+        hole_stats['S_mean_area_agg'][hr] = binned.S_mean_areaagg.iloc[h_loc]
+
+        hole_stats['N_mean_area'][hr], hole_stats['N_center_lat'][hr], hole_stats['N_center_lon'][hr] = areaint(
+            binned.N_lat_mean, binned.N_lon_mean)
+        hole_stats['N_max_area'][hr], _, _ = areaint(binned.N_lat_mean * u.deg - (sigma * binned.N_lat_std * u.deg),
+                                                     binned.N_lon_mean * u.deg)
+        hole_stats['N_min_area'][hr], _, _ = areaint(binned.N_lat_mean * u.deg + (sigma * binned.N_lat_std * u.deg),
+                                                     binned.N_lon_mean * u.deg)
+
+        hole_stats['S_mean_area'][hr], hole_stats['S_center_lat'][hr], hole_stats['S_center_lon'][hr] = areaint(
+            binned.S_lat_mean, binned.S_lon_mean)
+        hole_stats['S_max_area'][hr], _, _ = areaint(binned.S_lat_mean * u.deg - (sigma * binned.S_lat_std * u.deg),
+                                                     binned.S_lon_mean * u.deg)
+        hole_stats['S_min_area'][hr], _, _ = areaint(binned.S_lat_mean * u.deg + (sigma * binned.S_lat_std * u.deg),
+                                                     binned.S_lon_mean * u.deg)
+
+        hole_stats['Date'][hr] = PCH_Tools.hrot2date(hr).iso
+        hole_stats['Harvey_Rotation'][hr] = hr
+        print(np.int(((hr - all_stats.Harvey_Rotation.iloc[0]) / (
+                    all_stats.Harvey_Rotation.iloc[-1] - all_stats.Harvey_Rotation.iloc[0])) * 100.))
+
+    return hole_stats
+
+
+def remove_quantities(df):
+    # Return a dataframe without quantiies
+
+    for key in df.keys():
+        if isinstance(df[key].iloc[0], u.Quantity):
+            df[key] = np.array([itm.value for itm in df[key]])
+
+    return df
+
+
+def read_all_pch_df():
+    # Hardcoded shortcut
+    
+    aia171 = pd.read_pickle('/Users/mskirk/data/PCH_Project/AIA171.pkl')
+    aia193 = pd.read_pickle('/Users/mskirk/data/PCH_Project/AIA193.pkl')
+    aia211 = pd.read_pickle('/Users/mskirk/data/PCH_Project/AIA211.pkl')
+    aia304 = pd.read_pickle('/Users/mskirk/data/PCH_Project/AIA304.pkl')
+
+    eit171 = pd.read_pickle('/Users/mskirk/data/PCH_Project/EIT171.pkl')
+    eit195 = pd.read_pickle('/Users/mskirk/data/PCH_Project/EIT195.pkl')
+    eit304 = pd.read_pickle('/Users/mskirk/data/PCH_Project/EIT304.pkl')
+
+    euvi171 = pd.read_pickle('/Users/mskirk/data/PCH_Project/EUVI171.pkl')
+    euvi195 = pd.read_pickle('/Users/mskirk/data/PCH_Project/EUVI195.pkl')
+    euvi304 = pd.read_pickle('/Users/mskirk/data/PCH_Project/EUVI304.pkl')
+
+    swap174 = pd.read_pickle('/Users/mskirk/data/PCH_Project/swap174.pkl')
+
+    return aia171, aia193, aia211, aia304, eit171, eit195, eit304, euvi171, euvi195, euvi304, swap174
